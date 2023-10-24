69e2a524473ad3d697e7a8b4cf70f38c4e78c78d
Khussett <Hussett@gmail.com>
2023-10-24 18:48:57 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const searchBox = document.getElementById("searchBox");
+    const searchButton = document.getElementById("searchButton");
+    const clearButton = document.getElementById("clearButton");
+    const results = document.getElementById("results");
+
+    searchButton.addEventListener("click", () => {
+        searchData();
+    });
+
+    clearButton.addEventListener("click", () => {
+        clearResults();
+    });
+
+    searchBox.addEventListener("keyup", (event) => {
+        if (event.key === "Enter") {
+            searchData();
+        }
+    });
+
+    async function searchData() {
+        const searchTerm = searchBox.value;
+
+        if (searchTerm) {
+            // Define the API endpoint.
+            const apiUrl = `https://chroniclingamerica.loc.gov/search/titles/results/?terms=${encodeURIComponent(searchTerm)}&format=json`;
+
+            try {
+                // Fetch data from API asynchronously.
+                const response = await fetch(apiUrl);
+                const data = await response.json();
+
+                const limitedData = data.items.slice(0, 10);
+
+                // Process and display search results.
+                results.innerHTML = "";
+
+                limitedData.forEach(article => {
+                    const articleElement = document.createElement("div");
+                    articleElement.className = "article";
+                    articleElement.innerHTML = `
+                        <h2>${article.title}</h2>
+                        <p>Date: ${article.date}</p>
+                        <p>Place: ${article.place}</p>
+                    `;
+                    results.appendChild(articleElement);
+                });
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
+    }
+
+    function clearResults() {
+        results.innerHTML = "";
+        searchBox.value = "";
+    }
+});
